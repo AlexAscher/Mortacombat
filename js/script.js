b4487d946dfbdb24444a046f68c810a2e1026e4b
Alexey Ankudinov <alexascher8@gmail.com>
2024-09-08 17:23:34 +0300

    slick1commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$("document").ready(function() {
+    $('.carusel').slick({
+        arrows:true,
+        dots: true,
+        adaptiveHeight:true,
+        slidesToShow: 3,
+        speed:800,
+        autoplay:true,
+        autoPlaySpeed:5000,
+        centerMode:true,
+    });
+});

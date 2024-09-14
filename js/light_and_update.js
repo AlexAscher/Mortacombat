1b298fdaf64eb82bc5fbf62cfe84ad660acd09e7
Alexey Ankudinov <alexascher8@gmail.com>
2024-09-15 00:11:11 +0300

    забыл что делал

--- js/light_and_update.js
+++ js/light_and_update.js
@@ -25,28 +25,31 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function updateCarousel() {
         $('.carusel').slick('unslick'); // Удаляем предыдущую инициализацию
-        $('.carusel').slick({
-            arrows: true,
-            dots: true,
-            slidesToShow: 3,
-            speed: 800,
-            autoplay: true,
-            autoplaySpeed: 5000,
-            draggable: false,
-            swipe: false,
-            responsive: [{
-                    breakpoint: 768,
-                    settings: {
-                        slidesToShow: 2
+        $(document).ready(function () {
+            $('.carusel').slick({
+                arrows: true,
+                dots: true,
+                slidesToShow: 3,
+                speed: 800,
+                autoplay: true,
+                autoplaySpeed: 5000,
+                draggable: false,
+                swipe: false,
+                infinite: true,
+                responsive: [{
+                        breakpoint: 768,
+                        settings: {
+                            slidesToShow: 2
+                        }
+                    },
+                    {
+                        breakpoint: 576,
+                        settings: {
+                            slidesToShow: 1
+                        }
                     }
-                },
-                {
-                    breakpoint: 576,
-                    settings: {
-                        slidesToShow: 1
-                    }
-                }
-            ]
+                ]
+            });
         });
         startCarousel(); // Запускаем автопрокрутку после обновления карусели
     }
--- js/script.js
+++ js/script.js
@@ -1,25 +1,28 @@
 $(document).ready(function () {
-    $('.carusel').slick({
-        arrows: true,
-        dots: true,
-        slidesToShow: 3,
-        speed: 800,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        draggable: false,
-        swipe: false,
-        responsive: [{
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 2
+    $(document).ready(function () {
+        $('.carusel').slick({
+            arrows: true,
+            dots: true,
+            slidesToShow: 3,
+            speed: 800,
+            autoplay: true,
+            autoplaySpeed: 5000,
+            draggable: false,
+            swipe: false,
+            infinite: true,
+            responsive: [{
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 2
+                    }
+                },
+                {
+                    breakpoint: 576,
+                    settings: {
+                        slidesToShow: 1
+                    }
                 }
-            },
-            {
-                breakpoint: 576,
-                settings: {
-                    slidesToShow: 1
-                }
-            }
-        ]
+            ]
+        });
     });
 });

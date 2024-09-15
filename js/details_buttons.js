b84a0b50539482c4fd08853e0c394bf7adb39fd9
Alexey Ankudinov <alexascher8@gmail.com>
2024-09-15 15:57:19 +0300

    исправил скрипт с кнопками

--- js/details_buttons.js
+++ js/details_buttons.js
@@ -1,19 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
-    function toggleDetails(button, details) {
-        let areOpen = Array.from(details).every(detail => detail.open);
-        details.forEach(detail => detail.open = !areOpen);
-        button.textContent = areOpen ? "Показать Фильтр" : "Скрыть Фильтры";
-    }
-
     const buttons = document.querySelectorAll("#filter");
     const detailsElements = document.querySelectorAll(".firstdetail");
     const detailsElements2 = document.querySelectorAll(".firstdetail2");
 
-    buttons[0].addEventListener("click", function () {
-        toggleDetails(buttons[0], detailsElements);
-    });
+    function toggleDetails(button, details) {
+        let areAllOpen = Array.from(details).every(detail => detail.open);
+        details.forEach(detail => detail.open = !areAllOpen);
+        button.textContent = areAllOpen ? "Показать Фильтр" : "Скрыть Фильтры";
+    }
 
-    buttons[1].addEventListener("click", function () {
-        toggleDetails(buttons[1], detailsElements2);
+    function updateButtonText(button, details) {
+        let areAllOpen = Array.from(details).every(detail => detail.open);
+        button.textContent = areAllOpen ? "Скрыть Фильтры" : "Показать Фильтр";
+    }
+
+    buttons.forEach((button, index) => {
+        const details = index === 0 ? detailsElements : detailsElements2;
+
+        button.addEventListener("click", function () {
+            toggleDetails(button, details);
+        });
+
+        details.forEach(detail => {
+            detail.addEventListener("toggle", function () {
+                updateButtonText(button, details);
+            });
+        });
     });
 });

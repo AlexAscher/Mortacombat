4880f8be8f2f7855014a81323fda57fefdc482b2
Alexey Ankudinov <alexascher8@gmail.com>
2024-09-15 16:24:30 +0300

    обновил скрипт light

--- js/light_and_update.js
+++ js/light_and_update.js
@@ -25,31 +25,29 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function updateCarousel() {
         $('.carusel').slick('unslick'); // Удаляем предыдущую инициализацию
-        $(document).ready(function () {
-            $('.carusel').slick({
-                arrows: true,
-                dots: true,
-                slidesToShow: 3,
-                speed: 800,
-                autoplay: true,
-                autoplaySpeed: 5000,
-                draggable: false,
-                swipe: false,
-                infinite: true,
-                responsive: [{
-                        breakpoint: 768,
-                        settings: {
-                            slidesToShow: 2
-                        }
-                    },
-                    {
-                        breakpoint: 576,
-                        settings: {
-                            slidesToShow: 1
-                        }
+        $('.carusel').slick({
+            arrows: true,
+            dots: true,
+            slidesToShow: 3,
+            speed: 800,
+            autoplay: true,
+            autoplaySpeed: 5000,
+            draggable: false,
+            swipe: false,
+            infinite: true,
+            responsive: [{
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 2
                     }
-                ]
-            });
+                },
+                {
+                    breakpoint: 576,
+                    settings: {
+                        slidesToShow: 1
+                    }
+                }
+            ]
         });
         startCarousel(); // Запускаем автопрокрутку после обновления карусели
     }
@@ -91,11 +89,11 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     // Изначально скрываем элементы two и three
-
     two.classList.add('d-none');
     three.classList.add('d-none');
 
-
-    // Инициализируем карусель при загрузке страницы
-    updateCarousel();
+    // Инициализируем карусель после полной загрузки всех изображений
+    window.addEventListener('load', function () {
+        updateCarousel();
+    });
 });

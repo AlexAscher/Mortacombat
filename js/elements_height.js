597cc0ae14e92f1210d2658b0e877010e8ebd967
Alexey Ankudinov <alexascher8@gmail.com>
2024-10-27 12:48:06 +0300

    для бэкапа

--- js/elements_height.js
+++ js/elements_height.js
@@ -3,24 +3,32 @@ window.onload = () => {
     const tovari = document.querySelectorAll('.tovari');
     let maxHeight = 0;
     let maxHeighttovari = 0;
-    if (window.innerWidth > 768) {
-        elements.forEach(element => {
-            const elementHeight = element.offsetHeight;
-            if (elementHeight > maxHeight) {
-                maxHeight = elementHeight;
-            }
-        });
-        elements.forEach(element => {
-            element.style.height = `${maxHeight}px`;
-        });
-    }
+
+    // Определение максимальной высоты для .vsetovari
+    elements.forEach(element => {
+        const elementHeight = element.offsetHeight;
+        if (elementHeight > maxHeight) {
+            maxHeight = elementHeight;
+        }
+    });
+
+    // Установка максимальной высоты для всех .vsetovari
+    elements.forEach(element => {
+        element.style.height = `${maxHeight}px`;
+    });
+
+    // Определение максимальной высоты для .tovari
     tovari.forEach(element => {
         const tovariHeight = element.offsetHeight;
         if (tovariHeight > maxHeighttovari) {
             maxHeighttovari = tovariHeight;
         }
     });
+
+    // Добавление дополнительного пространства
     maxHeighttovari = maxHeighttovari + 30;
+
+    // Установка максимальной высоты для всех .tovari
     tovari.forEach(element => {
         element.style.height = `${maxHeighttovari}px`;
     });

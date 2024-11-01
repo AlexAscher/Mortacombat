00536b1af9407def32ca5126bd10eb0ac822e2b1
Alexey Ankudinov <alexascher8@gmail.com>
2024-11-01 20:57:28 +0300

    промежуточно форма +7 надо доделать почту

--- js/modal.js
+++ js/modal.js
@@ -52,7 +52,7 @@ function initializePopupHandlers() {
             const resultContent = form.closest('.pop_up_body').querySelector('.resultcontent'); // Находим контейнер для результатов
             const result = form.closest('.pop_up_body').querySelector('.result'); // Находим элемент для отображения результата
             let resultHTML = '<ul>'; // Начинаем создавать HTML для списка результатов
-            
+
             // Перебираем данные формы и формируем HTML
             formData.forEach((value, key) => {
                 if (value.trim()) { // Проверяем, не пустое ли значение
@@ -106,3 +106,36 @@ function showStopAlert(container) {
 
 // Инициализация обработчиков при загрузке страницы
 document.addEventListener('DOMContentLoaded', initializePopupHandlers); // Добавляем обработчик для загрузки страницы
+var inp = document.getElementById("phone");
+
+inp.onclick = function () {
+    inp.value = "+";
+}
+
+var old = 0;
+
+inp.onkeydown = function () {
+    var curLen = inp.value.length;
+
+    if (curLen < old) {
+        old--;
+        return;
+    }
+
+    if (curLen == 2)
+        inp.value = inp.value + "(";
+
+    if (curLen == 6)
+        inp.value = inp.value + ")-";
+
+    if (curLen == 11)
+        inp.value = inp.value + "-";
+
+    if (curLen == 14)
+        inp.value = inp.value + "-";
+
+    if (curLen > 16)
+        inp.value = inp.value.substring(0, inp.value.length - 1);
+
+    old++;
+}
